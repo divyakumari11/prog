1acfadbba7500ee3a0c7e5df4409bdef2a7749aa
divyakumari11 <48504113+divyakumari11@users.noreply.github.com>
2019-05-20 23:41:08 +0530

    Compare.c

---
+++ Compare.c
@@ -0,0 +1,26 @@
+include <stdio.h>
+
+/*function to get largest among three numbers*/
+
+int largestNumber(int a,int b, int c)
+{
+	int largest=0;
+	if(a>b && a>c)
+		largest=a;
+	else if(b>a && b>c)
+		largest=b;
+	else
+		largest=c;
+	return largest;
+}
+
+int main()
+{
+	int a,b,c;
+	
+	printf("Enter three numbers: ");
+	scanf("%d%d%d",&a,&b,&c);
+	printf("Largest number is: %d\n",largestNumber(a,b,c));
+	
+	return 0;
+}
